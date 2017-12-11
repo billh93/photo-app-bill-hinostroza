329d41ff48b0f3a12f33e14d6d8dd8f67ca6432c
Bill <billh93@gmail.com>
2017-12-11 10:51:58 -0600

    Moved sticky and dropmenu components to own files

--- src/App.js
+++ src/App.js
@@ -1,109 +1,14 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types';
-import DropdownMenu from 'react-dd-menu';
+import Sticky from './components/sticky.js';
+import DropMenu from './components/dropmenu.js';
 import "./App.css";
 
 // Global Variables
 // We will store our style for our gray background here
-// Look at line 119 for more info
+// Look at line 31 for more info
 let grayBackground = {};
 
 // TODO: Create tests using jests
-
-export class Sticky extends Component {
-  componentDidMount() {
-    const setInitialHeights = (elements) => {
-      [].forEach.call(elements, (sticky) => {
-        sticky.setAttribute('data-sticky-initial', sticky.getBoundingClientRect().top);
-      });
-    };
-
-    const stickies = document.querySelectorAll('[data-sticky]');
-    setInitialHeights(stickies);
-
-    document.addEventListener('scroll', () => {
-      const top = document.documentElement.scrollTop || document.body.scrollTop;
-      const bottom = document.documentElement.scrollHeight || document.body.scrollHeight;
-
-      [].forEach.call(stickies, (sticky) => {
-        const stickyInitial = parseInt(sticky.getAttribute('data-sticky-initial'), 10);
-        const stickyEnter = parseInt(sticky.getAttribute('data-sticky-enter'), 10) || stickyInitial;
-        const stickyExit = parseInt(sticky.getAttribute('data-sticky-exit'), 10) || bottom;
-
-        if (top >= stickyEnter && top <= stickyExit) {
-          sticky.classList.add('sticky');
-        } else {
-          sticky.classList.remove('sticky');
-        }
-      });
-    });
-  }
-
-  render() {
-    const { className, enter, exit, children } = this.props;
-    return (<div
-      className={`Sticky ${className}`}
-      data-sticky
-      data-sticky-enter={enter}
-      data-sticky-exit={exit}>
-      {children}
-    </div>);
-  }
-}
-
-Sticky.propTypes = {
-  className: PropTypes.string,
-  enter: PropTypes.string,
-  exit: PropTypes.string,
-  children: PropTypes.node,
-};
-
-class DropMenu extends Component {
-  constructor() {
-    super();
-    this.state = {
-        isMenuOpen: false
-    };
-    this.click = this.click.bind(this);
-    this.toggle = this.toggle.bind(this);
-    this.close = this.close.bind(this);
-  }
-
-  toggle() {
-    this.setState({ isMenuOpen: !this.state.isMenuOpen });
-  }
-
-  close() {
-    this.setState({ isMenuOpen: false });
-  }
-
-  click() {
-    console.log('You clicked an item');
-  }
-
-  render() {
-    const menuOptions = {
-      isOpen: this.state.isMenuOpen,
-      close: this.close,
-      toggle: <span onClick={this.toggle}>Menu</span>,
-      align: 'right'
-    };
-    return (
-      <DropdownMenu {...menuOptions}>
-        <li>Edit Profile</li>
-        <li>Account Settings</li>
-        <hr />
-        <li>Log Out</li>
-      </DropdownMenu>
-    );
-  }
-}
-
-DropMenu.propTypes = {
-  isOpen: PropTypes.bool,
-  close: PropTypes.bool,
-  toggle: PropTypes.func
-};
 
 class App extends Component {
     constructor(props){
