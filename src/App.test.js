af3913aee897a207a489767876119e087f84d2f0
Bill <billh93@gmail.com>
2017-12-11 12:10:09 -0600

    Added 2 render tests in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,13 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('Sticky renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Sticky />, div);
+});
+
+it('DropMenu renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<DropMenu />, div);
+});
